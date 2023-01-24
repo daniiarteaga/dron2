5c13c1206bc688c6901fb421e6384e1cca7091ff
daniiarteaga <90882299+daniiarteaga@users.noreply.github.com>
2023-01-24 12:05:39 -0500

    Update dron2.py

--- dron2.py
+++ dron2.py
@@ -4,35 +4,37 @@ import time, socket, exceptions, argparse
 #from sshkeyboard import listen_keyboard
 
 
-def connectMyCopter():
-    parser = argparse.ArgumentParser(description='commands')
-    parser.add_argument('--connect')
-    args = parser.parse_args()
+vehicle = connect('/dev/ttyAMA0',baud=921600,wait_ready=True)
+print(str(vehicle.system_status.state))
 
-    connection_string = args.connect
-    vehicle = connect(connection_string, wait_ready=True)
-
-    return vehicle
-
-
-def arm_and_takeoff(aTargetAltitude):
-    while not vehicle.is_armable:
+########
+def arm_and_takeoff(targetHeight):
+    while vehicle.is_armable!=True:
         print("Waiting for vehicle to become armable")
         time.sleep(1)
-    while vehicle.mode != "GUIDED":
-        print("Waiting for vehicle to enter GUIDED mode")
+    print("Vehicle is now armable.")
+    print("")
+
+    vehicle.mode = VehicleMode("GUIDED")
+    while vehicle.mode!='GUIDED':
+        print("Waiting for drone to enter GUIDED flight mode")
         time.sleep(1)
+    print("Vehicle is now in GUIDED mode.")
+
     vehicle.armed = True
-    while vehicle.armed == False:
-        print("Waiting for vehicle to become armed")
+    while vehicle.armed==False:
+        print("Waiting for vehicle to become armed.")
         time.sleep(1)
-    vehicle.simple_takeoff(aTargetAltitude)
+    print("Look out! Drone is armed and props are spinning!")
+    time.sleep(.5)
+
+    vehicle.simple_takeoff(targetHeight)
     while True:
-        print"Current Altitude: %d"vehicle.location.global_relative_frame.alt)
-        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * .95:
+        current_altitude = vehicle.location.global_relative_frame.alt
+        print("Current Altitude: "+str(current_altitude))
+        if current_altitude >=.95*targetHeight:
             break
         time.sleep(1)
-    print("Target altitude reached")
     return None
 
 
